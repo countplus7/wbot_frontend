7e2be7a3eb697af52687f509529f91adb768f197
countplus7 <countplus7@gmail.com>
2025-09-04 15:25:42 -0400

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,23 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    base: "/",
-    host: "::",
-    port: 8080,
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  base: "/wbot/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: "::",
+    port: 8080,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000", // dev backend
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 }));
